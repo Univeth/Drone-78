ee18670c672e7bc617842c656a56e7ffb6c26523
Ferb <fachmannf@gmail.com>
2024-04-27 11:19:52 +0200

    Code thatt works

---
+++ tello_commands.py
@@ -0,0 +1,21 @@
+import commands2
+
+
+class Tello:
+    def __init__(self) -> None:
+        self.command("command")
+
+    def command(self, command):
+        commands2.
+
+    def takeoff(self):
+        send_command("takeoff")
+
+    def land(self):
+        send_command("land")
+
+    def flip_forward(self):
+        send_command("flip f")
+
+    def flip_backward(self):
+        send_command("flip b")
